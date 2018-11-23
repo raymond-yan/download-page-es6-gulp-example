402a71472d27e891c6edf2775540e7d78a755189
raymond yan <raymond@coinx.pro>
2018-11-23 13:29:01 +0800

    Add build task to gulpfile.js using Minify CSS, Profix CSSand Unglify Js.

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,48 @@
-const babel = require('gulp-babel');
 const gulp = require('gulp');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
+const clean = require('gulp-clean');
 
-var sass = require('gulp-sass');
+const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
+const cssmin = require('gulp-cssnano'),
+    prefix = require('gulp-autoprefixer');
 
-gulp.task("transform", () => {
-    return gulp.src("main.js")
-        .pipe(babel())
-        .on("error", error => {
-            console.log(error);
-        })
-        .pipe(gulp.dest('dist'));
-});
+const hash = require('gulp-hash');
+const references = require('gulp-hash-references');
 
+const uglify = require('gulp-uglify'),
+    buffer = require('vinyl-buffer');
+
+var prefixerOptions = {
+    browsers: ['last 2 versions']
+};
+
+// DEV SUBTASKS
+// ---------------
 gulp.task('transform-runtime', () => {
     return browserify({
-            entries: "src/main.js",
+            entries: 'src/main.js',
             debug: true
         })
-        .transform("babelify", {
-            presets: ["@babel/preset-env"],
+        .transform('babelify', {
+            presets: ['@babel/preset-env'],
             plugins: ['@babel/plugin-proposal-class-properties']
         })
         .bundle()
-        .on("error", error => {
-            console.log("[Bundle Error] " + error);
+        .on('error', error => {
+            console.log('[Bundle Error] ' + error);
         })
-        .pipe(source('main3.js'))
-        .pipe(gulp.dest('dist'));
+        .pipe(source('main.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(hash.manifest('asset-manifest.json'))
+        .pipe(gulp.dest('.'));
 });
 
 gulp.task('copy', () => {
     gulp.src(['src/assets/**/*', '!src/assets/scss', '!src/assets/scss/*'])
         .pipe(gulp.dest('dist'));
+
     gulp.src('src/index.html')
         .pipe(gulp.dest('dist'));
 });
@@ -49,8 +57,73 @@ gulp.task('sass:watch', function () {
     gulp.watch('src/assets/scss/**/*.scss', ['sass']);
 });
 
-gulp.task("watch", () => {
-    return gulp.watch("src/**/*", ["transform-runtime", "copy"]);
+// DEV SUBTASKS
+// ---------------
+
+// BUILD SUBTASKS
+// ---------------
+
+gulp.task('clean', () => {
+    return gulp.src('dist', {
+            read: false
+        })
+        .pipe(clean());
+})
+
+gulp.task('transform', () => {
+    return browserify({
+            entries: 'src/main.js',
+            debug: false
+        })
+        .transform('babelify', {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+        })
+        .bundle()
+        .on('error', error => {
+            console.log('[Bundle Error] ' + error);
+        })
+        .pipe(source('main.js'))
+        // 添加 hash
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(hash())
+        .pipe(gulp.dest('dist'))
+        .pipe(hash.manifest('asset-manifest.json'))
+        .pipe(gulp.dest('.'));
 });
 
-gulp.task('default', ['transform-runtime', 'copy', 'watch', 'sass', 'sass:watch']);
+gulp.task('copy:build', () => {
+    gulp.src(['src/assets/**/*', '!src/assets/scss', '!src/assets/scss/*'])
+        .pipe(gulp.dest('dist'));
+});
+
+/** Update hte hash js or css to index.html */
+gulp.task('update-references', ['transform'], function () {
+    return gulp.src('src/index.html')
+        .pipe(references('asset-manifest.json')) // Replace file paths in index.html according to the manifest
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('sass:build', function () {
+    return gulp.src('src/assets/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(prefix(prefixerOptions))
+        .pipe(cssmin())
+        .pipe(gulp.dest('dist/css'));
+});
+
+// BUILD TASKS
+// ------------
+
+gulp.task('develop', ['transform-runtime', 'copy'])
+
+gulp.task('watch', () => {
+    return gulp.watch('src/**/*', ['develop']);
+});
+
+gulp.task('default', ['develop', 'watch', 'sass', 'sass:watch']);
+
+gulp.task('build', ['clean'], () => {
+    gulp.start(['transform', 'update-references', 'sass:build', 'copy:build']);
+});
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import {$on, resize, getParameterByName, getLang, setLang, getOperatingSystem} f
 import {IOSDownload} from './ios';
 import {AndroidDownload} from './android';
 
+const supportLangs = ['en', 'cn', 'hk'];
+
 $on(window, 'load', () => {
     resize();
     // 获取hash， 如 #/android?lang=cn
@@ -10,8 +12,11 @@ $on(window, 'load', () => {
     // 获取类型 ios / android, 从路由获取或者
     const type = route.split('?')[0] || getOperatingSystem();
     // 获取
-    const lang = getParameterByName('lang', route) || getLang();
-    // setLang(lang);
+    let lang = getParameterByName('lang', route) || getLang();
+    if (supportLangs.indexOf(lang) < 0) {
+        lang = supportLangs[0];
+    }
+    setLang(lang);
     if (type === 'ios') {
         const ios = new IOSDownload(lang);
         ios.init();
